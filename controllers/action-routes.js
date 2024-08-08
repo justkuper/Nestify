6218ea5732dde63cbf2bedad778432edf82a70fd
justkuper <kbank1988@gmail.com>
2024-08-08 11:02:20 -0700

    mostly done with action routes

--- controllers/action-routes.js
+++ controllers/action-routes.js
@@ -2,9 +2,61 @@ const router = require('express').Router();
 const { User, Provider, Ticket } = require('../models');
 const auth = require('../utils/auth');
 
-router.get('/profile/:id', auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
+  const providerData = await Provider.findAll().catch((err) => {
+    res.json(err);
+  });
+  const providers = providerData.map((provider) => provider.get({ plain: true }));
+  res.render('all', { providers });
+});
+
+router.get('/user/', auth, async (req, res) => {
   try {
-    res.render('profile');
+    const userData = await User.findByPk(req.params.id);
+    res.render('user');
+    } 
+    const user = userData.get({ plain: true });
+    res.render('user', user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/user/:id', auth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: 'No provider with this id!' });
+      return;
+    }
+    const user = userData.get({ plain: true });
+    res.render('user', user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/provider', auth, async (req, res) => {
+  try {
+    const providerData = await Provider.findByPk(req.params.id);
+    res.render('provider');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/provider/:id', auth, async (req, res) => {
+  try {
+    const providerData = await Provider.findByPk(req.params.id);
+    if (!providerData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    const provider = providerData.get({ plain: true });
+    res.render('provider', provider);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -13,6 +65,7 @@ router.get('/profile/:id', auth, async (req, res) => {
 
 router.get('/ticket/', auth, async (req, res) => {
   try {
+    const ticketData = await Ticket.findByPk(req.params.id);
     res.render('ticket');
   } catch (err) {
     console.log(err);
@@ -22,7 +75,13 @@ router.get('/ticket/', auth, async (req, res) => {
 
 router.get('/ticket/:id', auth, async (req, res) => {
   try {
-    res.render('ticket');
+    const ticketData = await Ticket.findByPk(req.params.id);
+    if (!ticketData) {
+      res.status(404).json({ message: 'No ticket with this id!' });
+      return;
+    }
+    const ticket = ticketData.get({ plain: true });
+    res.render('ticket', ticket);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
