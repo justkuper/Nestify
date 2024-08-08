2b85d78e2d6ce5cafd227586ad395c2afead710a
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-08 13:18:52 -0700

    fixed type const selector

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,11 @@
-const loginFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
     const username = document.querySelector("#username").value.trim();
     const password = document.querySelector("#password").value.trim();
     // radio buttons 
-    const type = document.querySelector('input[name="group3"]:checked').value
+    const type = document.querySelector('input[name="userType"]:checked').value
 
     if (username && password && type) {
         // Send a POST request to the API endpoint
