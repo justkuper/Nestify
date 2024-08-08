c545e8e96a28a68eadbe485e6b94842d5732bf0c
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-08 10:48:50 -0700

    added the users dashboard

--- controllers/account-routes.js
+++ controllers/account-routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const { User, Provider, Ticket } = require('../models');
 const auth = require('../utils/auth');
 
-// account view
-router.get('/account', auth, async (req, res) => {
+// account views
+// for both users and providers
+router.get('/users', auth, async (req, res) => {
   try {
-    res.render('accounts');
+    res.render('users');
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
