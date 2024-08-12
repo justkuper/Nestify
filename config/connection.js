933821fbfd226683726751a9f4f998b573f26257
justkuper <kbank1988@gmail.com>
2024-08-12 11:04:02 -0700

    added to render

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+let sequelize;
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-      }
-    );
-
-module.exports = sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
