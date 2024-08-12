38ebac3b9dbc2cdb43d4d5d3c86cc992089b51df
justkuper <kbank1988@gmail.com>
2024-08-12 11:18:26 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,7 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+
 let sequelize;
 
 if (process.env.DB_URL) {
@@ -6,10 +10,12 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'postgres',
     },
   );
 }
+
+module.exports = sequelize;
